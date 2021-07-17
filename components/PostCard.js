3d0dd1b6913779f3e67964c79dcdacd8cf9cd7c3
JinuCheon <woojin8787@gmail.com>
2021-07-17 14:13:35 +0900

    feat : post page function

--- components/PostCard.js
+++ components/PostCard.js
@@ -1,7 +1,62 @@
-const PostCard = () => {
+import { useState, useCallback } from 'react';
+import { Card, Popover, Button, Avatar} from 'antd';
+import { useSelector } from 'react-redux'
+import { RetweetOutlined, HeartOutlined, MessageOutlined, EllipsisOutlined, HeartTwoTone } from '@ant-design/icons';
+import PropTypes from 'prop-types';
+import PostImages from './PostImages';
+
+const PostCard = ({ post }) => {
+    const [liked, setLiked] = useState(false);
+    const [commentFormOpened, setcommentFormOpened] = useState(false);
+    const onToggleLike = useCallback(() => {
+        setLiked((prev) => !prev);
+    }, [])
+    const onToggleComment = useCallback(() => {
+        setcommentFormOpened((prev) => !prev);
+    })
+    const { me } =useSelector((state) => state.user);
+    const id = me && me.id;
     return (
-        <div>PostCard</div>
+        <div style={{ marginBotton: '20px'}}>
+            <Card
+                cover={post.Images[0] && <PostImages images={post.Images} />}
+                actions={[
+                    <RetweetOutlined key="retweet" />,
+                    liked
+                    ? <HeartTwoTone twoToneColor="#ff0000" key="heart" onClick={onToggleLike} />
+                    : <HeartOutlined key="heart" onClick={onToggleLike} />,
+                    <MessageOutlined key="comment" onClick={onToggleComment}/>,
+                    <Popover key="more" content={(
+                        <Button.Group>
+                            {id && post.User.id === id ? (
+                                <>
+                                    <Button>수정</Button>
+                                    <Button type="danger">삭제</Button>
+                                </>
+                            ) : <Button>신고</Button>}
+                        </Button.Group>
+                    )}>
+                        <EllipsisOutlined />
+                    </Popover>
+                ]} 
+            >
+                <Card.Meta 
+                    avatar={<Avatar>{post.User.nickname[0]}</Avatar>}
+                    title={post.User.nickname}
+                    description={post.content}
+                />
+            </Card>
+            {commentFormOpened && (
+                <div>댓글부분</div>
+            )}
+            {/* <CommentForm />
+            <Comments /> */}
+        </div>
     );
+};
+
+PostCard.propTypes = {
+    post: PropTypes.object.isRequired,
 };
 
 export default PostCard;
--- reducers/post.js
+++ reducers/post.js
@@ -5,29 +5,28 @@ export const initialState = {
             id: 1,
             nickname: '제로초',
         },
-        content: '첫 게시글 안녕하세요'
-    },],
-    Images: [{
-        src: 'https://images.chosun.com/resizer/HoGaPo0K-HNh_w9wmkUxpt404rc=/616x0/filters:focal(291x444:301x454)/cloudfront-ap-northeast-1.images.arcpublishing.com/chosun/XG2MW2H3ZRW5FHDVSOMF6FDT3E.jpg',
-    }, {
-        src: 'https://i.pinimg.com/564x/1d/e5/b1/1de5b1350ddf2f37ee536aec562680e3.jpg',
-    }, {
-        src: 'https://i.pinimg.com/564x/1d/e5/b1/1de5b1350ddf2f37ee536aec562680e3.jpg',
-    },
-    ],
-    Comments: [{
-        User: {
-            nickname: 'hero',
-        },
-        content: '안녕하세요 반가워요',
-    },
-    {
-        User: {
-            nickname: 'hero2',
-        },
-        content: '안녕하세요2 반가워요2',
+        content: '첫 게시글 안녕하세요 #해시태그',
+        Images: [{
+            src: 'https://images.chosun.com/resizer/HoGaPo0K-HNh_w9wmkUxpt404rc=/616x0/filters:focal(291x444:301x454)/cloudfront-ap-northeast-1.images.arcpublishing.com/chosun/XG2MW2H3ZRW5FHDVSOMF6FDT3E.jpg',
+        }, {
+            src: 'https://i.pinimg.com/564x/1d/e5/b1/1de5b1350ddf2f37ee536aec562680e3.jpg',
+        }, {
+            src: 'https://i.pinimg.com/564x/1d/e5/b1/1de5b1350ddf2f37ee536aec562680e3.jpg',
+        }],
+        Comments: [{
+            User: {
+                nickname: 'hero',
+            },
+            content: '안녕하세요 반가워요',
+        }, {
+            User: {
+                nickname: 'hero2',
+            },
+            content: '안녕하세요2 반가워요2',
+        }]
     }],
     imagePaths: [],
+    postAdded: false,
 }
 
 const ADD_POST = 'ADD_POST';
