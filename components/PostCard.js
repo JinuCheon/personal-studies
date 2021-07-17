78860be0fd375e83d0271720fd3b620e3b80edbf
JinuCheon <woojin8787@gmail.com>
2021-07-17 15:14:57 +0900

    feat : comment function

--- components/PostCard.js
+++ components/PostCard.js
@@ -1,9 +1,10 @@
 import { useState, useCallback } from 'react';
-import { Card, Popover, Button, Avatar} from 'antd';
+import { Card, Popover, Button, Avatar, List, Comment} from 'antd';
 import { useSelector } from 'react-redux'
 import { RetweetOutlined, HeartOutlined, MessageOutlined, EllipsisOutlined, HeartTwoTone } from '@ant-design/icons';
 import PropTypes from 'prop-types';
 import PostImages from './PostImages';
+import CommentForm from './CommentForm';
 
 const PostCard = ({ post }) => {
     const [liked, setLiked] = useState(false);
@@ -47,10 +48,24 @@ const PostCard = ({ post }) => {
                 />
             </Card>
             {commentFormOpened && (
-                <div>댓글부분</div>
+                <div>
+                    <CommentForm post={post} />
+                    <List
+                        header={`${post.Comments.length}개의 댓글`}
+                        itemLayout="horizontal"
+                        dataSource={post.Comments}
+                        renderItem={(item) => (
+                            <li>
+                                <Comment
+                                    author={item.User.nickname}
+                                    avatar={<Avatar>{item.User.nickname[0]}</Avatar>}
+                                    content={item.content}
+                                />
+                            </li>
+                        )}
+                    />
+                </div>
             )}
-            {/* <CommentForm />
-            <Comments /> */}
         </div>
     );
 };
