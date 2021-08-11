23ca9bab008844a2c541045816a0c940a506e3d1
JinuCheon <woojin8787@gmail.com>
2021-08-11 17:30:41 +0900

    day3 - 의존성 주입 (어노테이션 ver)

--- src/main/java/hello/hellospring/repository/MemberRepository.java
+++ src/main/java/hello/hellospring/repository/MemberRepository.java
@@ -1,10 +1,12 @@
 package hello.hellospring.repository;
 
 import hello.hellospring.domain.Member;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface MemberRepository {
     Member save(Member member); //domain에 있는 member 클래스이다.
 
