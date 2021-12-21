4b24778cc691fe089132943059e904d1c00f6b2e
JinuCheon <woojin8787@gmail.com>
2021-12-21 14:21:44 +0900

    feat : showing check list

--- scripts/to-do-list.js
+++ scripts/to-do-list.js
@@ -1,14 +1,14 @@
 let toDoListMemoryStorage = [];
-let numberToDoList=0;
+let sequenceCounter = 0;
 
 class ToDoList {
-    constructor(date, title, completedList, pendingList){
-        this.numberToDoList = numberToDoList++;
-        this.date = date;
-        this.title = title;
-        this.completedList = completedList;
-        this.pendingList = pendingList;
-    }
+  constructor(date, title, completedList, pendingList) {
+    this.sequence = sequenceCounter++;
+    this.date = date;
+    this.title = title;
+    this.completedList = completedList;
+    this.pendingList = pendingList;
+  }
 }
 
 let cardTemplate = (planData) => `
@@ -16,7 +16,7 @@ let cardTemplate = (planData) => `
     <div class="card-body">
         <h5 class="card-title">${planData.date} ${planData.title}</h5>
         <p class="card-text">진행률 : ${planData.completedList.length / (planData.completedList.length + planData.pendingList.length) * 100}%</p>
-        <button id="${this.numberToDoList}" type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">상세 보기</button>
+        <button onclick="openList(${planData.sequence})" type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">상세 보기</button>
     </div>
 </div>
 `
@@ -32,24 +32,49 @@ let createNewCardTemplate = () => `
 </div>
 `
 
+let checkFormList = (list) => {
+  let form = '';
+  for (let pending of list.pendingList) {
+    form += `
+      <div class="form-check">
+        <input class="form-check-input " type="checkbox" value="" id="flexCheckChecked" checked>
+        <label class="form-check-label" for="flexCheckChecked">${pending}</label>
+      </div>`
+  }
+  for (let completed of list.completedList) {
+    form += `
+      <div class="form-check">
+        <input class="form-check-input " type="checkbox" value="" id="flexCheckChecked">
+        <label class="form-check-label" for="flexCheckChecked">${completed}</label>
+      </div>`
+  }
+  return form
+}
+
+let openList = (sequence) => {
+  $planTitle = document.getElementById('planTitle');
+  $pendingListNumber = document.getElementById('pending-list-number');
+  $checkForms = document.getElementById('checkForms');
+  $checkForms.innerHTML = checkFormList(toDoListMemoryStorage[sequence])
+}
+
 let printList = () => {
-    let listWrapper = document.getElementById('list-wrapper');
-    console.log(listWrapper);
-    let tempTDL = '';
-    let cnt=0;
-    newTDL();
-    newTDL();
-    newTDL();
-    for(let planData of toDoListMemoryStorage){
-        tempTDL += cardTemplate(planData);
-    }
-    tempTDL += createNewCardTemplate();
-    listWrapper.innerHTML = tempTDL;
+  let $listWrapper = document.getElementById('list-wrapper');
+  console.log($listWrapper);
+  let tempTDL = '';
+  newTDL();
+  newTDL();
+  newTDL();
+  for (let planData of toDoListMemoryStorage) {
+    tempTDL += cardTemplate(planData);
+  }
+  tempTDL += createNewCardTemplate();
+  $listWrapper.innerHTML = tempTDL;
 }
 
 let newTDL = () => {
-    const toDoList = new ToDoList("12/17", "일정제목", ["a", "b", "c"], ["d", "e", "f"]);
-    toDoListMemoryStorage.push(toDoList);
+  const toDoList = new ToDoList("12/17", "일정제목", ["a", "b", "c"], ["d", "e", "f"]);
+  toDoListMemoryStorage.push(toDoList);
 }
 
 printList();
