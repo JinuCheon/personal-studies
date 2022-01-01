6e287fc25ac472eee088d3d2b3f2266d28ce086c
JinuCheon <woojin8787@gmail.com>
2022-01-02 08:25:46 +0900

    chore : import template's CSS and Javascript

--- main/erp-react-app/src/App.js
+++ main/erp-react-app/src/App.js
@@ -1,24 +1,53 @@
-import logo from './logo.svg';
+import { Helmet } from 'react-helmet';
 import './App.css';
+import './assets/css/style.css';
+import './assets/css/skin_color.css'
+import './assets/vendor_components/bootstrap/dist/css/bootstrap.min.css';
+// import './assets/vendor_components/bootstrap/datatable/datatables.min.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Helmet>
+        <script src="./assets/vendor_components/jquery-3.3.1/jquery-3.3.1.js"></script>
+        {/* <!-- fullscreen --> */}
+        <script src="./assets/vendor_components/screenfull/screenfull.js"></script>
+
+        {/* <!-- popper --> */}
+        <script src="./assets/vendor_components/popper/dist/popper.min.js"></script>
+
+        {/* <!-- Bootstrap 4.0--> */}
+        <script src="./assets/vendor_components/bootstrap/dist/js/bootstrap.min.js"></script>
+
+        {/* <!-- This is data table --> */}
+        <script src="./assets/vendor_components/datatable/datatables.min.js"></script>
+
+        {/* <!-- SlimScroll --> */}
+        <script src="./assets/vendor_components/jquery-slimscroll/jquery.slimscroll.min.js"></script>
+
+        {/* <!-- FastClick --> */}
+        <script src="./assets/vendor_components/fastclick/lib/fastclick.js"></script>
+
+        {/* <!-- sparkline --> */}
+        <script src="./assets/vendor_components/jquery-sparkline/dist/jquery.sparkline.js"></script>
+
+        {/* <!-- peity --> */}
+        <script src="./assets/vendor_components/jquery.peity/jquery.peity.js"></script>
+
+        {/* <!-- Pearl Admin App --> */}
+        <script src="./assets/js/template.js"></script>
+
+        {/* <!-- Pearl Admin for demo purposes --> */}
+        <script src="./assets/js/demo.js"></script>
+
+        {/* <!-- Pearl Admin for Data Table --> */}
+        <script src="./assets/js/pages/data-table.js"></script>
+
+        {/* <!-- demo only --> */}
+        <script src="./assets/js/pages/app-ticket.js"></script>
+      </Helmet>
+    </>
   );
 }
 
