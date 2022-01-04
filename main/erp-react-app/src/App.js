5939dfc19875ce32a294df89e60ccdd72c776644
JinuCheon <woojin8787@gmail.com>
2022-01-04 13:30:11 +0900

    feat : inventory management table

--- main/erp-react-app/src/App.js
+++ main/erp-react-app/src/App.js
@@ -17,51 +17,14 @@ function App() {
   return (
     <Router>
       <div className='hold-transition light-skin sidebar-mini theme-blackberry'>
-          <Header />
-          <Aside />
-          <Routes>
-            <Route path='/' element={<Home />} exact />
-            <Route path='/inventory-manage' element={<InventoryManage />} />
-          </Routes>
+        <Header />
+        <Aside />
+        <Routes>
+          <Route path='/' element={<Home />} exact />
+          <Route path='/inventory-manage' element={<InventoryManage />} />
+        </Routes>
       </div>
-      <Helmet>
-          <script type="text/babel" src="./assets/vendor_components/jquery-3.3.1/jquery-3.3.1.js"></script>
-          {/* <!-- fullscreen --> */}
-          <script type="text/babel" src="./assets/vendor_components/screenfull/screenfull.js"></script>
-
-          {/* <!-- popper --> */}
-          <script type="text/babel" src="./assets/vendor_components/popper/dist/popper.min.js"></script>
-
-          {/* <!-- Bootstrap 4.0--> */}
-          <script type="text/babel" src="./assets/vendor_components/bootstrap/dist/js/bootstrap.min.js"></script>
-
-          {/* <!-- This is data table --> */}
-          <script type="text/babel" src="./assets/vendor_components/datatable/datatables.min.js"></script>
-
-          {/* <!-- SlimScroll --> */}
-          <script type="text/babel" src="./assets/vendor_components/jquery-slimscroll/jquery.slimscroll.min.js"></script>
-
-          {/* <!-- FastClick --> */}
-          <script type="text/babel" src="./assets/vendor_components/fastclick/lib/fastclick.js"></script>
-
-          {/* <!-- sparkline --> */}
-          <script type="text/babel" src="./assets/vendor_components/jquery-sparkline/dist/jquery.sparkline.js"></script>
-
-          {/* <!-- peity --> */}
-          <script type="text/babel" src="./assets/vendor_components/jquery.peity/jquery.peity.js"></script>
-
-          {/* <!-- Pearl Admin App --> */}
-          <script type="text/babel" src="./assets/js/template.js"></script>
-
-          {/* <!-- Pearl Admin for demo purposes --> */}
-          <script type="text/babel" src="./assets/js/demo.js"></script>
-
-          {/* <!-- Pearl Admin for Data Table --> */}
-          <script type="text/babel" src="./assets/js/pages/data-table.js"></script>
-
-          {/* <!-- demo only --> */}
-          <script type="text/babel" src="./assets/js/pages/app-ticket.js"></script>
-        </Helmet>
+      
     </Router>
   );
 }
--- main/erp-react-app/src/components/Test1.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Test1 = () => {
-  return (
-    <h1>test1</h1>
-  );
-};
-
-export default Test1;
