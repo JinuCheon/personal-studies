80f6a6642e5fc920f22a30afa800efb3c0b15894
JinuCheon <woojin8787@gmail.com>
2022-05-25 23:53:17 +0900

    JPQL day1

---
+++ jpql/src/main/java/jpql/JpaMain.java
@@ -0,0 +1,33 @@
+package jpql;
+
+import javax.persistence.*;
+import java.util.List;
+
+public class JpaMain {
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+            Member member = new Member();
+            member.setUsername("member1");
+            member.setAge(10);
+            em.persist(member);
+
+            String jpql = "select m from Member m order by m.name desc";
+            List<Member> resultList = em.createQuery(jpql, Member.class)
+                    .setFirstResult(10)
+                    .setMaxResults(20)
+                    .getResultList();
+
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+        emf.close();
+    }
+}
